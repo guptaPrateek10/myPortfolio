19f873208f57e80ecb09cfa2f0e98288ec44693b
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-05-01 15:52:04 +0530

    Hero section Added

---
+++ src/pages/components/Hero.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
+import Image from "next/image";
+import mypic from "../../assets/mypic.jpg";
+type Props = {};
+
+const Hero = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: [" Prateek Gupta", "a React Developer"],
+    loop: true,
+    delaySpeed: 1000,
+    typeSpeed: 50,
+    deleteSpeed: 50,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center  ">
+      <BackgroundCircle />
+      <Image
+        className="relative rounded-full h-40 w-40 mx-auto object-cover"
+        src={mypic}
+        alt="Prateek's Photo"
+      />
+      <div className="z-20">
+        <h2 className=" text-sm-uppercase text-gray-500 pb-2 tracking-[15px] ">
+          Software Engineer
+        </h2>
+        <h1 className=" text-5xl lg:text-6xl font-semibold scroll-px-10 ">
+          <span className="dark:text-yellow-400 mr-3">Hi, I am {text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Hero from "./components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`dark:bg-gray-800 bg-white flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`dark:bg-gray-800 bg-white flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} snap-y  snap-mandatory  z-0`}
     >
       <Head>
         <title>Prateek's Portfolio</title>
       </Head>
 
       {/*Hero Section */}
-
+      <section id="hero" className="snap-center ">
+        <Hero />
+      </section>
       {/*Skills */}
 
       {/*Projects */}
