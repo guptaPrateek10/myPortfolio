fbc7e02475122b6fe1b38528c8edd4c0b8830030
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-05-02 18:55:56 +0530

    header changed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="header">
-      <div className="logo">Prateek's Portfolio</div>
+      <div className="logo">Prateek Gupta Portfolio</div>
       <div className="menu">
         <div className="web-menu">
           <Web />
