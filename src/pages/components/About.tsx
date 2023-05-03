5e44cc0d9e8192ae1585e5752231ec2092baea0a
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-05-04 00:51:17 +0530

    About section added

---
+++ src/pages/components/About.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { motion } from "framer-motion";
+import mypic from "../../assets/mypic.jpg";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex  flex-col relative h-screen text-center md:text-left md:flex-row
+    max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className=" top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        src={mypic.src}
+        width={300}
+        height={300}
+        alt="Prateek picture"
+        className="-mb-20 md:mb-0  rounded-full object-cover
+        flex-shrink-0 w-56 h-56 md:rounded-lg
+         md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] "
+      />
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#f7ab0a]/50"> Small</span>{" "}
+          Background
+        </h4>
+        <p className="text-base">
+          I'm Prateek.I'm a React JavaScript developer with 2.5 years of
+          experience. As a developer, I'm passionate about building responsive,
+          performant, and user-friendly web applications using React. I have a
+          strong understanding of the React ecosystem, including Redux, React
+          Router, and other related technologies. I'm also comfortable working
+          with other front-end technologies such as HTML, CSS, and JavaScript.
+          Throughout my career, I've collaborated with designers, product
+          owners, and other developers to deliver high-quality software products
+          on time and within budget. I'm comfortable working in an Agile
+          development environment and I value clear communication, team
+          collaboration, and a focus on delivering value to the end-user. In
+          addition to my technical skills, I'm a lifelong learner who's always
+          looking to improve my skills and stay up-to-date with the latest
+          industry trends. I'm also a strong problem solver who enjoys tackling
+          complex challenges and finding elegant solutions.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,27 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`dark:bg-gray-800 bg-white flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} snap-y  snap-mandatory  z-0`}
+      className={`dark:bg-gray-800 bg-white h-screen flex flex-col  
+        ${inter.className} snap-y  snap-mandatory z-0`}
     >
       <Head>
         <title>Prateek's Portfolio</title>
       </Head>
 
       {/*Hero Section */}
-      <section id="hero" className="snap-center ">
+      <section id="hero" className="snap-start">
         <Hero />
+      </section>
+      {/*About */}
+      <section id="about" className="snap-center">
+        <About />
       </section>
       {/*Skills */}
 
