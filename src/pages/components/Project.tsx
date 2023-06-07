7dc9c28795e18ea24be12c1e6cc3ce81dd37ad62
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-06-08 01:51:30 +0530

    Project Section design completed

--- src/pages/components/Project.tsx
+++ src/pages/components/Project.tsx
@@ -12,7 +12,7 @@ const Project = () => {
       transition={{ duration: 1.5 }}
       className="h-full relative flex overflow-hidden flex-col text-left mb-5 md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap">
